40d07ff19f47a3d647eb12df9da57cad5604712a
radhikakevat <radhikakevat1995@gmail.com>
2020-08-21 18:29:44 +0530

    Updates

---
+++ precache-manifest.24f7bb4f7f56d580c56a5086c35c4cbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48c9c089fc33816a89925d2cdfd8126",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "edaf21b10be0ba975a59",
+    "url": "/covid-19/static/css/main.fe5a0f3b.chunk.css"
+  },
+  {
+    "revision": "586362933d24821a5234",
+    "url": "/covid-19/static/js/2.51c25330.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19/static/js/2.51c25330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edaf21b10be0ba975a59",
+    "url": "/covid-19/static/js/main.9e25ee9f.chunk.js"
+  },
+  {
+    "revision": "d46fff10edf104623014",
+    "url": "/covid-19/static/js/runtime-main.6f57abcd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
